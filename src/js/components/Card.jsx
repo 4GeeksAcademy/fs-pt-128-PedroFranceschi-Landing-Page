3ca0ac767ad54fd539c92dbee18c25e5e8528316
Pedro Franceschi <117317386+FranceschiPedro@users.noreply.github.com>
2025-12-16 10:41:48 +0000

    Patricia... Ya no me fastidies mas, gracias

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,59 +1,30 @@
 import React from "react";
 
 const Card = () => {
+
+    const myArray = ["1", "2", "1", "1",];
+
     return (
         // style={{"width: 18rem;"}}
         <>
             <div className="container-fluid text-center">
                 <div className="row justify-content-center">
-                    <div className="col-10 col-sm-6 col-lg-3 d-flex justify-content-center">
-                        <div className="card mb-5" >
-                            <img src="./src/img/rigo-baby.jpg" className="card-img-top" alt=".." />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            </div>
-                            <div className="card-body">
-                                <a href="#" className="card-link">Card link</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-10 col-sm-6 col-lg-3 d-flex justify-content-center">
-                        <div className="card mb-5" >
-                            <img src="./src/img/rigo-baby.jpg" className="card-img-top" alt=".." />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            </div>
-                            <div className="card-body">
-                                <a href="#" className="card-link">Card link</a>
+
+                    { myArray.map((card, index) => (
+                        <div key={index} className="col-10 col-sm-6 col-lg-3 d-flex justify-content-center">
+                            <div className="card mb-5" >
+                                <img src="./src/img/rigo-baby.jpg" className="card-img-top" alt=".." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title </h5>
+                                    <p className="card-text">{card}</p>
+                                </div>
+                                <div className="card-body">
+                                    <a href="#" className="card-link">Card link</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-10 col-sm-6 col-lg-3 d-flex justify-content-center">
-                        <div className="card mb-5" >
-                            <img src="./src/img/rigo-baby.jpg" className="card-img-top" alt=".." />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            </div>
-                            <div className="card-body">
-                                <a href="#" className="card-link">Card link</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-10 col-sm-6 col-lg-3 d-flex justify-content-center">
-                        <div className="card mb-5" >
-                            <img src="./src/img/rigo-baby.jpg" className="card-img-top" alt=".." />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            </div>
-                            <div className="card-body">
-                                <a href="#" className="card-link">Card link</a>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
+
                 </div>
             </div>
         </>
